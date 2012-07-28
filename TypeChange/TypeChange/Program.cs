7c8e4db78586262952ea7b96bc957454372f5682
seasun <jianghaoyuane4711@gmail.com>
2012-07-29 00:01:42 +0800

    this is my the first init project,just for test

---
+++ TypeChange/TypeChange/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TypeChange
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] pers = new Person[2] {
+            new Person{Name="seasun",Age=25},
+              new Person{Name="candy",Age=24}
+            };
+            foreach (Person p in pers)
+            {
+                Console.WriteLine("姓名：{0}，年龄：{1}", p.Name, p.Age);
+            }
+            Console.Read();
+        }
+    }
+}
