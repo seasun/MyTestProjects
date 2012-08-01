975ba12c8e6a64e878886f970d30aacd901ddf49
seasun <jianghaoyuane4711@gmail.com>
2012-08-01 18:39:57 +0800

    add observer project

---
+++ MyManageProject/Observer/Observer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Observer
+{
+    public delegate EventHandler Notify();
+    class Observer
+    {
+    }
+}
