2e86f2d3855ed549c4949cfc05af74254491224d
seasun <jianghaoyuane4711@gmail.com>
2012-08-03 18:31:54 +0800

    add Prototype pattern 2012-8-3 18:31

---
+++ MyManageProject/PrototypePattern/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+
+namespace PrototypePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Resume one = new Resume("seasun", 26, "memego");
+            ShowResume(one);
+            ShowResume((Resume)one.Clone());
+            Console.Read();
+        }
+
+        private static void ShowResume(Resume resume)
+        {
+            var properties = typeof(Resume).GetProperties(BindingFlags.Instance | BindingFlags.Public);
+            foreach (var property in properties)
+            {
+                Console.WriteLine("{0} is {1}", property.Name, property.GetValue(resume, null));
+            }
+            Console.WriteLine("**************************************************************");
+        }
+    }
+}
---
+++ MyManageProject/PrototypePattern/Resume.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PrototypePattern
+{
+    class Resume : ICloneable
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Company { get; set; }
+
+        public Resume() { }
+        public Resume(string name, int age, string company = "")
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Company = company;
+        }
+
+        #region ICloneable 成员
+
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+
+        #endregion
+    }
+}
