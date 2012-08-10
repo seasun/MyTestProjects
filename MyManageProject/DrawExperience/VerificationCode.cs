209b7a6efa4848a935989ab587f34ea605e6d832
seasun <jianghaoyuane4711@gmail.com>
2012-08-10 14:26:56 +0800

    add DrawExperience of project by 2012-8-10 14:26

---
+++ MyManageProject/DrawExperience/VerificationCode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DrawExperience
+{
+    public class VerificationCode
+    {
+        //public uint[] RandomNums(uint codeNum = 5)
+        //{
+        //    codeNum = codeNum > 20 ? 20 : codeNum;
+        //    uint[] randoms = new uint[codeNum];
+        //    Random rand = new Random();
+        //    for (int i = 0; i < codeNum; codeNum++) {
+               
+        //    }
+        //}
+    }
+}
