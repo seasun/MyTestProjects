d9ad0612d2fc21876c55afe8f747144f568a2fde
seasun <jianghaoyuane4711@gmail.com>
2012-11-16 00:06:10 +0800

    add wcf test

---
+++ MyManageProject/Hosting/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Hosting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ MyManageProject/Service.Interface/Icalculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+
+namespace Service.Interface
+{
+    [ServiceContract(Name = "CalculatorService", Namespace = "http://www.seasun.com/")]
+    public interface Icalculator
+    {
+        //要显式指定“服务操作”
+        [OperationContract]
+        double Add(double x, double y);
+        [OperationContract]
+        double Subtract(double x, double y);
+        [OperationContract]
+        double Multiply(double x, double y);
+        [OperationContract]
+        double Divide(double x, double y);
+    }
+}
